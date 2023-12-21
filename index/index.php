af7e3dfd3d194e08bcbf7153fb38fbc9d56e03a4
angel <79240498+evilgrave6@users.noreply.github.com>
2023-12-21 15:18:37 +0900

    Update index.php

--- index/index.php
+++ index/index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Заголовок страницы</title>
     <link rel="stylesheet" href="./styles/style.css">
-
   </head>
   <body>
     <header>
